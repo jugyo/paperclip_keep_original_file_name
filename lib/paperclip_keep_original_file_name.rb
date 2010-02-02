4c3eccc5a13f4153bdea45ba015a2d37ca7a9640
jugyo <kkohno@gmail.com>
2010-02-02 16:16:19 +0900

    fixed a bug

--- lib/paperclip_keep_original_file_name.rb
+++ lib/paperclip_keep_original_file_name.rb
@@ -2,7 +2,7 @@ module Paperclip
   class Attachment
     def assign_with_set_original_filename(file)
       assign_without_set_original_filename(file)
-      instance_write(:original_file_name, file.original_filename)
+      instance_write(:original_file_name, file.original_filename) if file
     end
     alias_method_chain :assign, :set_original_filename
   end
